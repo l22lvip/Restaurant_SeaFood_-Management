2f4c159d3bf5bd6dd26c954566675b785a69014a
duythaidev <nguyenduythai17092005@gmail.com>
2025-07-08 20:51:13 +0700

    feat: add CRUD employee page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Auth, Orders } from './pages';
 import Header from './components/shared/Header';
 import BottomNav from './components/shared/BottomNav';
+import EmployeeManagement from './pages/EmployeeManagement';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/orders" element={<Orders />} />
+          <Route path="/employees" element={<EmployeeManagement />} />
         </Routes>
         <BottomNav />
       </Router>
