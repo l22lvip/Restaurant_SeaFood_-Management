45f444cb44c4aebebbd8772505f0cc24353a77fe
khanh2005fpt <nguyenhuubaokhanh2005@gmail.com>
2025-07-17 00:14:19 +0700

    code create bill

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Auth, Orders, Tables } from './pages';
 import Header from './components/shared/Header';
 import BottomNav from './components/shared/BottomNav';
+import CreateBill from './components/bill/CreateBill';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/auth" element={<Auth />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/tables" element={<Tables />} />
+          <Route path="/create-bill" element={<CreateBill />} />
         </Routes>
         <BottomNav />
       </Router>
